e910b4cbefa245af5f14081994fbe784030ff733
Herman Perez <herman.perez@omni.cr>
2022-07-05 09:46:32 -0600

    feat(base): initial configuration and unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders My Challenge App text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wellcomeText = screen.getByText(/My Challenge App/i);
+  expect(wellcomeText).toBeInTheDocument();
 });
